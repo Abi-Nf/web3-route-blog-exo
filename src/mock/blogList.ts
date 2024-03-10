5f3f442cd143863ccbd9acd8f7caaf460fd71fcc
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-03-10 14:53:07 +0300

    feat: blog list

---
+++ src/mock/blogList.ts
@@ -0,0 +1,15 @@
+export interface BlogItem {
+  // image path
+  path: string;
+  content: string;
+}
+
+export const blogList: BlogItem[] = [
+  { path: '/books.png', content: 'A book' },
+  { path: '/books.png', content: 'Imagine something else apart a book (1)' },
+  { path: '/books.png', content: 'Imagine something else apart a book (2)' },
+  { path: '/books.png', content: 'Imagine something else apart a book (3)' },
+  { path: '/books.png', content: 'Imagine something else apart a book (4)' },
+  { path: '/books.png', content: 'Imagine something else apart a book (5)' },
+  { path: '/books.png', content: 'Imagine something else apart a book (6)' },
+]
